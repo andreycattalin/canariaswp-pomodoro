68ca35552bf1bfd1df4c1e7699ec533aa57752c6
Andrey <cattalin.htc@gmail.com>
2023-01-19 16:22:49 +0100

    init

---
+++ main.js
@@ -0,0 +1,58 @@
+let isActive = false;
+let timeRemaining = 25 * 60;
+var xTimer;
+var audio = new Audio("alarm.mp3");
+
+function activateTimer() {
+  if (isActive) {
+    document.getElementById("btn-start").classList.remove("hidden");
+    document.getElementById("btn-stop").classList.add("hidden");
+    isActive = false;
+    clearInterval(xTimer);
+  } else {
+    document.getElementById("btn-stop").classList.remove("hidden");
+    document.getElementById("btn-start").classList.add("hidden");
+    isActive = true;
+
+    startTimer();
+  }
+}
+
+function startTimer() {
+  xTimer = setInterval(function () {
+    timeRemaining--;
+    console.log(timeRemaining);
+
+    let minutes = Math.floor(timeRemaining / 60);
+    let seconds = timeRemaining % 60;
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    let myTime = minutes + ":" + seconds;
+
+    if (timeRemaining <= 10) {
+      document
+        .getElementById("timer")
+        .classList.replace("text-white", "text-red-500");
+    }
+
+    if (timeRemaining <= 0) {
+      audio.play();
+      clearInterval(xTimer);
+      timeRemaining = 25 * 60;
+      document
+        .getElementById("timer")
+        .classList.replace("text-red-500", "text-white");
+      document.getElementById("timer").innerHTML = "25:00";
+      activateTimer();
+
+      setTimeout(function () {
+        audio.pause();
+      }, 2000);
+      return;
+    }
+
+    document.getElementById("timer").innerHTML = myTime;
+  }, 1000); // 1000 ms es cada 1 segundo
+}
